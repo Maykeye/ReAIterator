e890bd9430abbff5521da6e79c404a1c1b66b78c
Maykeye <Maykeye@users.noreply.github.com>
2023-08-10 12:02:39 +0600

    Decoupling main loop from gptq

--- reAIterator.py
+++ reAIterator.py
@@ -1,10 +1,8 @@
 from tqdm.auto import tqdm
 import os
 from pathlib import Path
-from transformers import AutoTokenizer, pipeline, logging
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 
-STORY = "/tmp/prompt.txt"
+STORY = "/tmp/prompt.ptxt"
 ACTUAL_PROMPT = f"{STORY}.act"
 MODEL_ID_PATH = os.path.expanduser("~/models/Nous-Hermes-Llama2-GPTQ")
 USE_TRITON = False
@@ -17,6 +15,11 @@ MARKER = ";;;"
 MARKER_SKIP = f"{MARKER}-"
 MARKER_QUIT = f"{MARKER}---"
 
+# Back-end commands
+CMD_INIT = "init"
+CMD_TOKEN_COUNT = "token-count"
+CMD_GENERATE = "generate"
+
 
 def export_prompt():
     global prompt
@@ -28,7 +31,37 @@ def export_prompt():
     prompt = Path(f"{STORY}").read_text()
 
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID_PATH)
+def gptq(cmd, prompt=None, state={}):
+    from auto_gptq import AutoGPTQForCausalLM
+    from transformers import AutoTokenizer
+    if cmd == CMD_INIT:
+        assert prompt is None
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_ID_PATH)
+        model = AutoGPTQForCausalLM.from_quantized(
+            MODEL_ID_PATH,
+            model_basename=MODEL_BASENAME,
+            use_safetensors=True,
+            trust_remote_code=False,
+            device="cuda:0",
+            use_triton=USE_TRITON,
+            quantize_config=None)
+        state["tokenizer"] = tokenizer
+        state["model"] = model
+        return
+
+    tokenizer, model = state["tokenizer"], state["model"]
+    if cmd == CMD_TOKEN_COUNT:
+        assert prompt is not None
+        return len(tokenizer(prompt).input_ids)
+    if cmd == CMD_GENERATE:
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+        outs = model.generate(
+            inputs=input_ids, do_sample=True,
+            temperature=TEMPERATURE, max_new_tokens=N_TOKENS)
+        return tokenizer.decode(outs[0], skip_special_tokens=True)
+    raise ValueError(f"Unknown command {cmd}")
+
+
 if Path(STORY).exists():
     prompt = Path(STORY).read_text()
 else:
@@ -37,26 +70,19 @@ else:
     prompt += "### Response:\n"
 reconstructed_prompt = prompt
 
-model = AutoGPTQForCausalLM.from_quantized(
-    MODEL_ID_PATH,
-    model_basename=MODEL_BASENAME,
-    use_safetensors=True,
-    trust_remote_code=False,
-    device="cuda:0",
-    use_triton=USE_TRITON,
-    quantize_config=None)
-
 
 def reconstruct_prompt(whole_prompt):
-    parts = whole_prompt.split(";;;")
+    parts = whole_prompt.split(MARKER)
     res = ""
     for part in parts:
         part = part.removeprefix("\n")
         if part.startswith("---"):
             break
+        if part.startswith("-^"):
+            res = res.removesuffix("\n")
+            continue
         if part.startswith("-"):
             continue
-
         if part.startswith('^'):
             part = part[1:]
             res = res.removesuffix("\n")
@@ -64,25 +90,24 @@ def reconstruct_prompt(whole_prompt):
     return res
 
 
+backend = gptq
+backend(CMD_INIT)
+
 export_prompt()
 while True:
     reconstructed_prompt = reconstruct_prompt(prompt)
-    initial_len = len(reconstructed_prompt)
-    input_ids = tokenizer(reconstructed_prompt, return_tensors='pt').input_ids.cuda()
-    if input_ids.shape[1] > PROMPT_LEN_TO_SPLIT:
-        input(f"vvv SPLIT vvv [promptlen: {input_ids.shape[1]}/{PROMPT_LEN_TO_SPLIT}")
+    n_tokens = backend(CMD_TOKEN_COUNT, prompt=reconstructed_prompt)
+    if n_tokens > PROMPT_LEN_TO_SPLIT:
+        input(f"vvv SPLIT vvv [promptlen: {n_tokens}/{PROMPT_LEN_TO_SPLIT}")
         export_prompt()
         continue
 
-    print(f"Prompt length: {input_ids.shape[1]}")
-    start = input_ids.shape[1]
+    print(f"Prompt length: {n_tokens}")
+    start = len(reconstructed_prompt)
     outs = [""]
     for _ in tqdm(range(N_GENS), desc="Generations"):
-        output = model.generate(inputs=input_ids,
-                                do_sample=True,
-                                temperature=TEMPERATURE,
-                                max_new_tokens=N_TOKENS)
-        outs.append(tokenizer.decode(output[0][start:], skip_special_tokens=True))
+        generated = backend(CMD_GENERATE, prompt=reconstructed_prompt)
+        outs.append(generated[start:])
 
     outs = "\n~~~v~~~\n".join(outs)
     if MARKER_QUIT in prompt:
